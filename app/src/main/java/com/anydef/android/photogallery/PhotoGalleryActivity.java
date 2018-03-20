247cdb5b700df349277e449bf17103249409fd4d
ilbcj <ilbcj@hotmail.com>
2018-03-20 15:48:03 +0800

    chap 25 finish

---
+++ app/src/main/java/com/anydef/android/photogallery/PhotoGalleryActivity.java
@@ -0,0 +1,16 @@
+package com.anydef.android.photogallery;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalleryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        Fragment fragment = PhotoGalleryFragment.newInstance();
+        return fragment;
+    }
+
+}
+
